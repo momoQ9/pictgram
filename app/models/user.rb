35d15b30eff0fc37c043ca7215156615755d5924
Kiyomiya Takuma <Takuma@momo.base.com>
2019-06-09 22:49:03 +0900

    パスワードをアルファベット、数字の混合のみ可能に設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
   validates :password, length: {in: 8..32}
-
-
+  validates :password, format: {with: /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}$+/z}
+  validates :password, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z}
   validates :name, length: {in: 1..15}
   has_secure_password
 end
