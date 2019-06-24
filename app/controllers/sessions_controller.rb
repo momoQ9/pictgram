7dd9c036a2aadf96b03da0a8bf016c03b709cd31
Kiyomiya Takuma <Takuma@momo.base.com>
2019-06-25 08:52:34 +0900

    SessionsControllerにストロングパラメーターを実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :password)
   end
   def log_in(user)
     session[:user_id] = user.id
